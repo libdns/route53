7194c9dc145dc0b20462187cb9e5a0abd7e40425
PJ <peterjan.brone@gmail.com>
2022-03-18 14:14:47 +0100

    Do not overwrite TTL

--- provider.go
+++ provider.go
@@ -46,7 +46,6 @@ func (p *Provider) AppendRecords(ctx context.Context, zone string, records []lib
 		if err != nil {
 			return nil, err
 		}
-		newRecord.TTL = time.Duration(newRecord.TTL) * time.Second
 		createdRecords = append(createdRecords, newRecord)
 	}
 
@@ -68,7 +67,6 @@ func (p *Provider) DeleteRecords(ctx context.Context, zone string, records []lib
 		if err != nil {
 			return nil, err
 		}
-		deletedRecord.TTL = time.Duration(deletedRecord.TTL) * time.Second
 		deletedRecords = append(deletedRecords, deletedRecord)
 	}
 
@@ -90,7 +88,6 @@ func (p *Provider) SetRecords(ctx context.Context, zone string, records []libdns
 		if err != nil {
 			return nil, err
 		}
-		updatedRecord.TTL = time.Duration(updatedRecord.TTL) * time.Second
 		updatedRecords = append(updatedRecords, updatedRecord)
 	}
 
